6fa8e76dcf87e4f7e94c2a01b2f762b614c1ae4f
hyeongonkim <simonkim@kookmin.ac.kr>
2020-04-05 22:00:26 +0900

    상품판매 제외 구현

--- src/com/simonkim/Admin.java
+++ src/com/simonkim/Admin.java
@@ -7,7 +7,7 @@ import java.io.*;
 
 public class Admin {
 
-  String logPath = "/Users/simonkim/Desktop/OOAD_POST/OOAD_log.txt";
+  static String logPath = "/Users/simonkim/Desktop/OOAD_POST/OOAD_log.txt";
   String dataPath = "/Users/simonkim/Desktop/OOAD_POST/OOAD_data.txt";
   String pcPath = "/Users/simonkim/Desktop/OOAD_POST/PC/";
 
@@ -26,37 +26,43 @@ public class Admin {
     totalAmount = 0;
     menuTable = new HashMap<String, Integer>();
 
-    writeLog("관리자 포스가 켜졌습니다.", logPath);
+    writeLog("포스가 켜졌습니다.");
   }
 
   void init() throws IOException {
-    writeLog("초기 환경설정을 시작합니다...", logPath);
+    writeLog("초기 환경설정을 시작합니다...");
 
     readData(dataPath);
-    writeLog("시간당 이용 금액은 " + Integer.toString(chargeFee) + "원 입니다.", logPath);
-    writeLog("판매 메뉴는 " + String.valueOf(menuTable.keySet()) + "입니다.", logPath);
+    writeLog("시간당 이용 금액은 " + Integer.toString(chargeFee) + "원 입니다.");
+    writeLog("판매 메뉴는 " + String.valueOf(menuTable.keySet()) + "입니다.");
 
-    refreshPC();
-    writeLog("PC 10대 동기화가 완료되었습니다.", logPath);
-    writeLog("초기 환경설정이 끝났습니다...", logPath);
+    for (int i = 1; i <= 10; i++) {
+      BufferedReader bufReader = new BufferedReader(
+          new FileReader(pcPath + Integer.toString(i) + ".txt"));
+      userPCs[i - 1][0] = bufReader.readLine();
+      userPCs[i - 1][1] = bufReader.readLine();
+      bufReader.close();
+    }
+    writeLog("PC 10대 동기화가 완료되었습니다.");
+    writeLog("초기 환경설정이 끝났습니다...");
   }
 
   void refreshPC() throws IOException {
     for (int i = 1; i <= 10; i++) {
-      BufferedReader bufReader = new BufferedReader(
-          new FileReader( pcPath + Integer.toString(i) + ".txt"));
-      userPCs[i - 1][0] = bufReader.readLine();
-      userPCs[i - 1][1] = bufReader.readLine();
-      bufReader.close();
-      if(!userPCs[i - 1][1].equals("0") && userPCs[i - 1][0].equals("1")) {
+      if (!userPCs[i - 1][1].equals("0") && userPCs[i - 1][0].equals("1")) {
         int reTime = Integer.parseInt(userPCs[i - 1][1]);
         reTime--;
-        if(reTime <= 0) {
+        if (reTime <= 0) {
           writePCStatus("0", "0", Integer.toString(i), pcPath);
         } else {
           writePCStatus(userPCs[i - 1][0], Integer.toString(reTime), Integer.toString(i), pcPath);
         }
       }
+      BufferedReader bufReader = new BufferedReader(
+          new FileReader(pcPath + Integer.toString(i) + ".txt"));
+      userPCs[i - 1][0] = bufReader.readLine();
+      userPCs[i - 1][1] = bufReader.readLine();
+      bufReader.close();
     }
   }
 
@@ -65,15 +71,18 @@ public class Admin {
   }
 
   void chargePC(int target, int time, int receiveMoney) throws IOException {
-    PCcharged(target, (time * chargeFee / 60) / 10 * 10); // 1원 단위 버림
-    writeLog(Integer.toString(target) + "번 PC 충전 [" + Integer.toString(receiveMoney) + "원 수령 / " + Integer.toString((time * chargeFee / 60) / 10 * 10) + "원 결제 / " + Integer.toString(receiveMoney - (time * chargeFee / 60) / 10 * 10) + "원 거스름] 결제되었습니다.", logPath);
+    PCcharged(target, time); // 1원 단위 버림
+    writeLog(
+        Integer.toString(target) + "번 PC 충전 [" + Integer.toString((time * chargeFee / 60) / 10 * 10)
+            + "원 수령 / " + Integer.toString((time * chargeFee / 60) / 10 * 10) + "원 결제 / " + Integer
+            .toString(receiveMoney - (time * chargeFee / 60) / 10 * 10) + "원 거스름] 결제되었습니다.");
   }
 
   int calMenu(String[] order) throws IOException {
     //상품 판매, 주문을 연속해서 받고 결제버튼을 누르면 합산 가격 출력 및 결제(receive로 받은 현금, 거스름돈 출력)
     //상품 가액 계산
     int price = 0;
-    for(int i = 0; i < order.length; i++) {
+    for (int i = 0; i < order.length; i++) {
       price += menuTable.get(order[i]);
     }
     return price;
@@ -82,16 +91,17 @@ public class Admin {
   void payMenu(String[] order, int price, int receiveMoney) throws IOException {
     totalAmount += price;
     String menu = "";
-    for(int i = 0; i < order.length - 1; i++) {
+    for (int i = 0; i < order.length - 1; i++) {
       menu += order[i] + ", ";
     }
     menu += order[order.length - 1];
-    writeLog("상품 주문 [<" + menu + "> 주문 / " + Integer.toString(receiveMoney) + "원 수령 / " + Integer.toString(price) + "원 결제 / " + Integer.toString(receiveMoney - price) + "원 거스름] 결제되었습니다.", logPath);
+    writeLog("상품 주문 [<" + menu + "> 주문 / " + Integer.toString(receiveMoney) + "원 수령 / " + Integer
+        .toString(price) + "원 결제 / " + Integer.toString(receiveMoney - price) + "원 거스름] 결제되었습니다.");
   }
 
-  static void writeLog(String input, String path) throws IOException {
+  static void writeLog(String input) throws IOException {
     long nowTime = System.currentTimeMillis();
-    PrintWriter log = new PrintWriter(new FileWriter(path, true));
+    PrintWriter log = new PrintWriter(new FileWriter(logPath, true));
     String text = time.format(nowTime) + input;
     log.println(text);
     System.out.println(text);
@@ -118,7 +128,17 @@ public class Admin {
     bufReader.close();
   }
 
-  void PCcharged(int target, int pay) throws IOException {
+  void writeData() throws IOException {
+    PrintWriter statusFile = new PrintWriter(new FileWriter(dataPath));
+    statusFile.println(chargeFee);
+    menuTable.forEach((key, value) -> {
+      statusFile.println(key);
+      statusFile.println(value);
+    });
+    statusFile.close();
+  }
+
+  void PCcharged(int target, int time) throws IOException {
 
     BufferedReader bufReader = new BufferedReader(
         new FileReader(pcPath + Integer.toString(target) + ".txt"));
@@ -126,12 +146,13 @@ public class Admin {
     int nowTime = Integer.parseInt(bufReader.readLine());
     bufReader.close();
 
-    totalAmount += pay;
-    String chargedTime = Integer.toString(nowTime + (int)((double)pay/chargeFee*60));
+    totalAmount += (time * chargeFee / 60) / 10 * 10;
+    String chargedTime = Integer.toString(nowTime + time);
     userPCs[target - 1][0] = "1";
     userPCs[target - 1][1] = chargedTime;
     writePCStatus("1", chargedTime, Integer.toString(target), pcPath);
-    writeLog(Integer.toString(target) + "번 PC에 " + chargedTime + "분을 충전하였습니다. (" + Integer.toString(pay) + "원)", logPath);
+    writeLog(Integer.toString(target) + "번 PC에 " + time + "분을 충전하였습니다. (" + Integer
+        .toString((time * chargeFee / 60) / 10 * 10) + "원)");
   }
 
   void writePCStatus(String status, String remainTime, String pcNum, String path)
--- src/com/simonkim/GUIproc.java
+++ src/com/simonkim/GUIproc.java
@@ -5,36 +5,374 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class GUIproc {
+
+  static int forChargePCNum = 0;
+  static int forChargePCTime = 0;
+
   public static void main(String[] args) throws IOException {
     JFrame frame = new JFrame("PC cafe");
-    frame.setSize(800, 600);
+    frame.setSize(900, 600);
     frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    Admin post = new Admin();
+
+    JPanel chargePage = new JPanel();
+    JPanel mainPage = new JPanel();
+    JPanel payPage = new JPanel();
+    JPanel sellPage = new JPanel();
+    JPanel chgPCPayPage = new JPanel();
+    JPanel salesPage = new JPanel();
+
+    // 충전요금변경 패널
+
+    chgPCPayPage.setLayout(new GridLayout(7, 3));
+
+    JPanel nowPay = new JPanel();
+    nowPay.setLayout(new BoxLayout(nowPay, BoxLayout.X_AXIS));
+    nowPay.add(new JLabel("현재 이용 요금 : "));
+    JLabel nowMoney = new JLabel("");
+    nowPay.add(nowMoney);
+
+    JPanel newPay = new JPanel();
+    newPay.setLayout(new BoxLayout(newPay, BoxLayout.X_AXIS));
+    newPay.add(new JLabel("바꿀 이용 요금 : "));
+    JTextField newMoney = new JTextField();
+    newPay.add(newMoney);
+
+    chgPCPayPage.add(nowPay);
+    chgPCPayPage.add(newPay);
+
+    JButton changePay = new JButton("변경하기");
+    chgPCPayPage.add(changePay);
+
+    JButton returnChangeMain = new JButton("돌아가기");
+    chgPCPayPage.add(returnChangeMain);
+
+    returnChangeMain.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        newMoney.setText("");
+        frame.remove(chgPCPayPage);
+        frame.add(mainPage, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    changePay.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (Integer.parseInt(newMoney.getText()) >= 0) {
+          post.chargeFee = Integer.parseInt(newMoney.getText());
+          try {
+            post.writeData();
+            Admin.writeLog("PC이용요금을 " + Integer.toString(post.chargeFee) + "원으로 변경하였습니다.");
+          } catch (IOException ex) {
+            ex.printStackTrace();
+          }
+          newMoney.setText("");
+          frame.remove(chgPCPayPage);
+          frame.add(mainPage, BorderLayout.CENTER);
+          frame.revalidate();
+          frame.repaint();
+        }
+      }
+    });
+
+    // 충전요금변경 패널 끝
+
+    // 매출조회 패널
+
+    salesPage.setLayout(new GridLayout(7, 3));
+
+    JPanel nowSales = new JPanel();
+    nowSales.setLayout(new BoxLayout(nowSales, BoxLayout.X_AXIS));
+    nowSales.add(new JLabel("현재까지의 매출 : "));
+    JLabel nowSalesMoney = new JLabel("");
+    nowSales.add(nowSalesMoney);
+
+    salesPage.add(nowSales);
+
+    JButton returnSalesMain = new JButton("돌아가기");
+    salesPage.add(returnSalesMain);
+
+    returnSalesMain.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        nowSalesMoney.setText("");
+        frame.remove(salesPage);
+        frame.add(mainPage, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    // 매출조회 패널 끝
+
+    // 메인 패널
+
+    mainPage.setLayout(new GridLayout(5, 5));
+
+    mainPage.add(new JLabel("PC01", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC02", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC03", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC04", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC05", SwingConstants.CENTER));
+
+    JLabel pc01_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc01_Time);
+    JLabel pc02_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc02_Time);
+    JLabel pc03_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc03_Time);
+    JLabel pc04_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc04_Time);
+    JLabel pc05_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc05_Time);
+
+    mainPage.add(new JLabel("PC06", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC07", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC08", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC09", SwingConstants.CENTER));
+    mainPage.add(new JLabel("PC10", SwingConstants.CENTER));
+
+    JLabel pc06_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc06_Time);
+    JLabel pc07_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc07_Time);
+    JLabel pc08_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc08_Time);
+    JLabel pc09_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc09_Time);
+    JLabel pc10_Time = new JLabel("null", SwingConstants.CENTER);
+    mainPage.add(pc10_Time);
+
+    JButton pcCharge = new JButton("PC충전");
+    mainPage.add(pcCharge);
+    JButton menuSell = new JButton("상품판매");
+    mainPage.add(menuSell);
+    JButton pcFeeChange = new JButton("PC요금변경");
+    mainPage.add(pcFeeChange);
+    JButton inquirySales = new JButton("매출조회");
+    mainPage.add(inquirySales);
+    JButton sellOff = new JButton("판매종료");
+    mainPage.add(sellOff);
+
+    pcCharge.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.remove(mainPage);
+        frame.add(chargePage, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    pcFeeChange.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.remove(mainPage);
+        frame.add(chgPCPayPage, BorderLayout.CENTER);
+        nowMoney.setText(Integer.toString(post.chargeFee));
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    inquirySales.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.remove(mainPage);
+        frame.add(salesPage, BorderLayout.CENTER);
+        nowSalesMoney.setText(Integer.toString(post.totalAmount));
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    sellOff.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        try {
+          Admin.writeLog("판매를 종료합니다. 금번 매출은 " + Integer.toString(post.totalAmount) + "원 입니다.");
+        } catch (IOException ex) {
+          ex.printStackTrace();
+        }
+        System.exit(0);
+      }
+    });
+
+    // 메인 패널 끝
+
+    // 결제 패널
+
+    payPage.setLayout(new GridLayout(7, 3));
+
+    JPanel forPay = new JPanel();
+    forPay.setLayout(new BoxLayout(forPay, BoxLayout.X_AXIS));
+    forPay.add(new JLabel("결제할 금액 : "));
+    JLabel payMoney = new JLabel("");
+    forPay.add(payMoney);
+
+    JPanel recPay = new JPanel();
+    recPay.setLayout(new BoxLayout(recPay, BoxLayout.X_AXIS));
+    recPay.add(new JLabel("수령한 금액 : "));
+    JTextField recMoney = new JTextField();
+    recPay.add(recMoney);
+
+    payPage.add(forPay);
+    payPage.add(recPay);
+
+    JButton calPay = new JButton("수령확인 / 거스름계산");
+    payPage.add(calPay);
+
+    JPanel chgPay = new JPanel();
+    chgPay.setLayout(new BoxLayout(chgPay, BoxLayout.X_AXIS));
+    chgPay.add(new JLabel("거스름 금액 : "));
+    JLabel chgMoney = new JLabel("");
+    chgPay.add(chgMoney);
+    payPage.add(chgPay);
+
+    JButton successPay = new JButton("결제완료");
+    successPay.setEnabled(false);
+    payPage.add(successPay);
+
+    JButton cancel = new JButton("취소하기");
+    payPage.add(cancel);
+
+    cancel.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        payMoney.setText("");
+        recMoney.setText("");
+        chgMoney.setText("");
+        successPay.setEnabled(false);
+        frame.remove(payPage);
+        frame.add(mainPage, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    calPay.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        int requirePay = Integer.parseInt(payMoney.getText());
+        int paidPay = Integer.parseInt(recMoney.getText());
+        if (paidPay < requirePay) {
+          chgMoney.setText("금액부족, 결제불가능");
+        } else {
+          chgMoney.setText(Integer.toString(paidPay - requirePay));
+          successPay.setEnabled(true);
+        }
+      }
+    });
+
+    successPay.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        try {
+          post.chargePC(forChargePCNum, forChargePCTime, Integer.parseInt(recMoney.getText()));
+          refreshMain(post, pc01_Time, pc02_Time, pc03_Time, pc04_Time, pc05_Time, pc06_Time,
+              pc07_Time, pc08_Time, pc09_Time, pc10_Time);
+          payMoney.setText("");
+          recMoney.setText("");
+          chgMoney.setText("");
+          frame.remove(payPage);
+          frame.add(mainPage, BorderLayout.CENTER);
+          frame.revalidate();
+          frame.repaint();
+        } catch (IOException ex) {
+          ex.printStackTrace();
+        }
+      }
+    });
+
+    // 결제 패널 끝
+
+    // 충전 패널
+
+    chargePage.setLayout(new GridLayout(7, 3));
+
+    JPanel pcNum = new JPanel();
+    pcNum.setLayout(new BoxLayout(pcNum, BoxLayout.X_AXIS));
+    pcNum.add(new JLabel("충전할 번호 : "));
+    JTextField toNum = new JTextField();
+    pcNum.add(toNum);
+
+    JPanel pcTime = new JPanel();
+    pcTime.setLayout(new BoxLayout(pcTime, BoxLayout.X_AXIS));
+    pcTime.add(new JLabel("충전할 시간 : "));
+    JTextField toTime = new JTextField();
+    pcTime.add(toTime);
+
+    chargePage.add(pcNum);
+    chargePage.add(pcTime);
+
+    JButton payment = new JButton("결제하기");
+    chargePage.add(payment);
+
+    JButton returnChargeMain = new JButton("돌아가기");
+    chargePage.add(returnChargeMain);
+
+    returnChargeMain.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        toNum.setText("");
+        toTime.setText("");
+        frame.remove(chargePage);
+        frame.add(mainPage, BorderLayout.CENTER);
+        frame.revalidate();
+        frame.repaint();
+      }
+    });
+
+    payment.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        int temp1 = Integer.parseInt(toNum.getText());
+        int temp2 = Integer.parseInt(toTime.getText());
+        if (temp1 >= 1 && temp1 <= 10 && temp2 >= 1) {
+          forChargePCNum = temp1;
+          forChargePCTime = temp2;
+          toNum.setText("");
+          toTime.setText("");
+          payMoney.setText(Integer.toString(post.calChargePC(forChargePCTime)));
+          frame.remove(chargePage);
+          frame.add(payPage, BorderLayout.CENTER);
+          frame.revalidate();
+          frame.repaint();
+        }
+      }
+    });
+
+    // 충전 패널 끝
+
     // 판매시작 패널
 
-    JPanel sellPage = new JPanel();
     sellPage.setLayout(new GridLayout(1, 1));
 
     JButton sellOn = new JButton("판매를 시작하려면 눌러주세요");
     sellPage.add(sellOn);
-
-    Admin post = new Admin();
 
     sellOn.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
         try {
           post.init();
-        } catch (IOException ex) {
+          Thread.sleep(500);
+          refreshMain(post, pc01_Time, pc02_Time, pc03_Time, pc04_Time, pc05_Time, pc06_Time,
+              pc07_Time,
+              pc08_Time, pc09_Time, pc10_Time);
+        } catch (IOException | InterruptedException ex) {
           ex.printStackTrace();
         }
         frame.remove(sellPage);
+        frame.add(mainPage, BorderLayout.CENTER);
         frame.revalidate();
         frame.repaint();
       }
@@ -46,6 +384,61 @@ public class GUIproc {
 
     frame.setVisible(true);
 
+  }
+
+  private static void refreshMain(Admin post, JLabel pc01_Time, JLabel pc02_Time, JLabel pc03_Time,
+      JLabel pc04_Time, JLabel pc05_Time, JLabel pc06_Time, JLabel pc07_Time, JLabel pc08_Time,
+      JLabel pc09_Time, JLabel pc10_Time) {
+    if (post.userPCs[0][0].equals("0")) {
+      pc01_Time.setText("사용대기");
+    } else {
+      pc01_Time.setText("사용중 : " + post.userPCs[0][1] + "분 남음");
+    }
+    if (post.userPCs[1][0].equals("0")) {
+      pc02_Time.setText("사용대기");
+    } else {
+      pc02_Time.setText("사용중 : " + post.userPCs[1][1] + "분 남음");
+    }
+    if (post.userPCs[2][0].equals("0")) {
+      pc03_Time.setText("사용대기");
+    } else {
+      pc03_Time.setText("사용중 : " + post.userPCs[2][1] + "분 남음");
+    }
+    if (post.userPCs[3][0].equals("0")) {
+      pc04_Time.setText("사용대기");
+    } else {
+      pc04_Time.setText("사용중 : " + post.userPCs[3][1] + "분 남음");
+    }
+    if (post.userPCs[4][0].equals("0")) {
+      pc05_Time.setText("사용대기");
+    } else {
+      pc05_Time.setText("사용중 : " + post.userPCs[4][1] + "분 남음");
+    }
+    if (post.userPCs[5][0].equals("0")) {
+      pc06_Time.setText("사용대기");
+    } else {
+      pc06_Time.setText("사용중 : " + post.userPCs[5][1] + "분 남음");
+    }
+    if (post.userPCs[6][0].equals("0")) {
+      pc07_Time.setText("사용대기");
+    } else {
+      pc07_Time.setText("사용중 : " + post.userPCs[6][1] + "분 남음");
+    }
+    if (post.userPCs[7][0].equals("0")) {
+      pc08_Time.setText("사용대기");
+    } else {
+      pc08_Time.setText("사용중 : " + post.userPCs[7][1] + "분 남음");
+    }
+    if (post.userPCs[8][0].equals("0")) {
+      pc09_Time.setText("사용대기");
+    } else {
+      pc09_Time.setText("사용중 : " + post.userPCs[8][1] + "분 남음");
+    }
+    if (post.userPCs[9][0].equals("0")) {
+      pc10_Time.setText("사용대기");
+    } else {
+      pc10_Time.setText("사용중 : " + post.userPCs[9][1] + "분 남음");
+    }
   }
 
   static boolean minuteChangeChk() {
